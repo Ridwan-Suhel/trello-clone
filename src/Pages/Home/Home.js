04e00eacb3e0d28ea61fe139d0a6677982533d70
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-07-15 22:11:09 +0600

    init commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageNav from "../../components/PageNav/PageNav";
+
+const Home = () => {
+  return (
+    <div className="bg-[#0079BF] px-2">
+      <PageNav />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/PageNav/PageNav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNav = () => {
+  return (
+    <div className="py-1">
+      <h2 className="text-white font-bold text-2xl">Kanban Board</h2>
+    </div>
+  );
+};
+
+export default PageNav;

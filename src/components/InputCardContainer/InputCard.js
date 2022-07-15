64956a70ad1754106ca5904be2f8d84dc350026c
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-07-16 02:29:38 +0600

    add card button and add input card onclick toggle added

--- src/components/InputCardContainer/InputCard.js
+++ src/components/InputCardContainer/InputCard.js
@@ -1,7 +1,7 @@
 import { XIcon } from "@heroicons/react/solid";
 import React from "react";
 
-const InputCard = () => {
+const InputCard = ({ open, setOpen }) => {
   return (
     <div>
       <input
@@ -10,10 +10,16 @@ const InputCard = () => {
         placeholder="Enter a title for this card..."
       />
       <div className="flex gap-2 items-center">
-        <button className="py-2 px-3 bg-[#0079BF] text-sm hover:bg-[#026AA7] transition-all duration-300 rounded-md text-white">
+        <button
+          onClick={() => setOpen(!open)}
+          className="py-2 px-3 bg-[#0079BF] text-sm hover:bg-[#026AA7] transition-all duration-300 rounded-md text-white"
+        >
           Add Card
         </button>
-        <XIcon className="cursor-pointer w-6 h-6 text-gray-500 hover:text-gray-900 transition-all duration-300" />
+        <XIcon
+          onClick={() => setOpen(!open)}
+          className="cursor-pointer w-6 h-6 text-gray-500 hover:text-gray-900 transition-all duration-300"
+        />
       </div>
     </div>
   );
--- src/components/InputCardContainer/InputCardContainer.js
+++ src/components/InputCardContainer/InputCardContainer.js
@@ -1,12 +1,23 @@
 import { PlusIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useState } from "react";
 import InputCard from "./InputCard";
 
 const InputCardContainer = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <InputCard />
-      <div className="group flex gap-2 items-center px-2 cursor-pointer py-2 text-gray-700 hover:text-black hover:bg-gray-400 transition-all duration-300 rounded-sm ">
+      <div className={`${open ? "block" : "hidden"}`}>
+        <InputCard open={open} setOpen={setOpen} />
+      </div>
+      <div
+        onClick={() => setOpen(!open)}
+        className={`
+        ${
+          open ? "hidden" : "flex"
+        } group gap-2 items-center px-2 cursor-pointer py-2 text-gray-700 hover:text-black hover:bg-gray-400 transition-all duration-300 rounded-sm 
+        `}
+      >
         <PlusIcon className="w-5 h-5 group-hover:text-black" />
         <p className="text-lg">Add a card</p>
       </div>
